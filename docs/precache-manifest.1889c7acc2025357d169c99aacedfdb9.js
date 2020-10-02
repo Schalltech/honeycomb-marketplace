31cd008f506f03f05cf8dfa61bb3264228488e20
schalltech <eric@microapp.studio>
2020-10-02 06:41:58 +0000

    docs(readme): semaphore - updated root readme [skip ci]

---
+++ docs/precache-manifest.1889c7acc2025357d169c99aacedfdb9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188853d90309297b58ed14d38e03be55",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a69f750cc00dde2d8770.bundle.js"
+  },
+  {
+    "url": "runtime~main.a69f750cc00dde2d8770.bundle.js"
+  },
+  {
+    "url": "vendors~main.a69f750cc00dde2d8770.bundle.js"
+  },
+  {
+    "url": "vendors~main.a69f750cc00dde2d8770.bundle.js.LICENSE.txt"
+  }
+]);
